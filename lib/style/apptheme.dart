eb4357102e135bedc759e705f25ae00a5d70e612
JANNIES <janniesvarghese@gmail.com>
2025-07-14 23:45:40 +0530

    color

--- lib/style/apptheme.dart
+++ lib/style/apptheme.dart
@@ -1,15 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/style/appcolors.dart';
+import 'package:portfolio/style/apppadding.dart';
 
 class AppTheme {
-  static ThemeData get darkTheme => ThemeData(
-    colorScheme: ColorScheme.dark(
-      primary: AppColors.primaryColor,
-      surface: AppColors.darkBackgroundColor,
-      onSurface: AppColors.grey[100]!,
-    ),
-    useMaterial3: true,
-    scaffoldBackgroundColor: AppColors.darkBackgroundColor,
-    appBarTheme: AppBarTheme(color: AppColors.grey[900]),
-  );
+  final String fontFamily = "JetBrainsMono";
+
+  ThemeData get darkTheme {
+    return _getThemeData(
+      colorScheme: ColorScheme.dark(
+        primary: AppColors.primaryColor,
+        background: AppColors.darkBackgroundColor,
+        surface: AppColors.grey[850]!,
+        outline: AppColors.grey[800]!,
+        outlineVariant: AppColors.grey[700]!,
+        onBackground: AppColors.grey[100]!,
+        onSurface: AppColors.grey[300]!,
+        onSurfaceVariant: AppColors.grey[400]!,
+        tertiary: AppColors.grey[900]!,
+      ),
+      elevatedButtonTextStyle: _darkElevatedButtonTextStyle,
+      outlinedButtonTextStyle: _darkOutlinedButtonTextStyle,
+      scaffoldBackgroundColor: AppColors.darkBackgroundColor,
+      appBarTheme: AppBarTheme(color: AppColors.grey[900]),
+    );
+  }
+
+  ThemeData get lightTheme {
+    return _getThemeData(
+      colorScheme: ColorScheme.light(
+        primary: AppColors.primaryColor,
+        background: AppColors.grey[100]!,
+        surface: AppColors.grey[200]!,
+        outline: AppColors.grey[300]!,
+        outlineVariant: AppColors.grey[400]!,
+        onBackground: AppColors.grey[800]!,
+        onSurface: AppColors.grey[700]!,
+        onSurfaceVariant: AppColors.grey[600]!,
+        tertiary: AppColors.grey[900]!,
+      ),
+      elevatedButtonTextStyle: _lightElevatedButtonTextStyle,
+      outlinedButtonTextStyle: _lightOutlinedButtonTextStyle,
+      scaffoldBackgroundColor: AppColors.grey[100]!,
+      appBarTheme: AppBarTheme(color: AppColors.grey[900]),
+    );
+  }
+
+  ThemeData _getThemeData({
+    required ColorScheme colorScheme,
+    required WidgetStateProperty<TextStyle> elevatedButtonTextStyle,
+    required WidgetStateProperty<TextStyle> outlinedButtonTextStyle,
+    required Color scaffoldBackgroundColor,
+    required AppBarTheme appBarTheme,
+  }) {
+    return ThemeData(
+      colorScheme: colorScheme,
+      useMaterial3: true,
+      fontFamily: fontFamily,
+      scaffoldBackgroundColor: scaffoldBackgroundColor,
+      appBarTheme: appBarTheme,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          fixedSize: const WidgetStatePropertyAll(Size.fromHeight(40)),
+          backgroundColor: _primaryButtonStates,
+          padding: WidgetStatePropertyAll(
+            EdgeInsets.symmetric(horizontal: Insets.med, vertical: 10),
+          ),
+          textStyle: elevatedButtonTextStyle,
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: ButtonStyle(
+          fixedSize: const WidgetStatePropertyAll(Size.fromHeight(40)),
+          side: _outlinedButtonStates,
+          padding: WidgetStatePropertyAll(
+            EdgeInsets.symmetric(horizontal: Insets.med, vertical: 10),
+          ),
+          textStyle: outlinedButtonTextStyle,
+        ),
+      ),
+    );
+  }
+
+  final _primaryButtonStates = WidgetStateProperty.resolveWith((states) {
+    if (states.contains(WidgetState.hovered) ||
+        states.contains(WidgetState.pressed)) {
+      return const Color(0xFF229518).withValues(alpha: 0.7);
+    }
+    return AppColors.primaryColor;
+  });
+
+  final _outlinedButtonStates = WidgetStateProperty.resolveWith((states) {
+    if (states.contains(WidgetState.hovered) ||
+        states.contains(WidgetState.pressed)) {
+      return BorderSide(color: const Color(0xFF229518).withValues(alpha: 0.7));
+    }
+    return BorderSide(color: AppColors.primaryColor);
+  });
+
+  WidgetStatePropertyAll<TextStyle> get _darkElevatedButtonTextStyle =>
+      WidgetStatePropertyAll(
+        TextStyle(
+          color: AppColors.grey[100],
+          fontFamily: fontFamily,
+          fontWeight: FontWeight.w500,
+        ),
+      );
+
+  WidgetStatePropertyAll<TextStyle> get _lightElevatedButtonTextStyle =>
+      WidgetStatePropertyAll(
+        TextStyle(
+          color: AppColors.grey[800],
+          fontFamily: fontFamily,
+          fontWeight: FontWeight.w500,
+        ),
+      );
+
+  WidgetStatePropertyAll<TextStyle> get _darkOutlinedButtonTextStyle =>
+      WidgetStatePropertyAll(
+        TextStyle(
+          color: AppColors.grey[100],
+          fontFamily: fontFamily,
+          fontWeight: FontWeight.w500,
+        ),
+      );
+
+  WidgetStatePropertyAll<TextStyle> get _lightOutlinedButtonTextStyle =>
+      WidgetStatePropertyAll(
+        TextStyle(
+          color: AppColors.grey[800],
+          fontFamily: fontFamily,
+          fontWeight: FontWeight.w500,
+        ),
+      );
 }

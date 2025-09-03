b7f407ed34400a5a32da7e85f691ad686a055b1a
JANNIES <janniesvarghese@gmail.com>
2025-09-03 20:46:54 +0530

    gmail

---
+++ lib/features/home/presentation/gmail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Gmail extends StatefulWidget {
+  const Gmail({super.key});
+
+  @override
+  State<Gmail> createState() => _GmailState();
+}
+
+class _GmailState extends State<Gmail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
